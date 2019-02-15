b618627305c2a664671785ead6acd86331f3d277
cwong2015 <sirhc8002@gmail.com>
2019-02-15 11:04:47 -0500

    hackiness for figuring out if float... gonna commit then rewrite

--- q2.py
+++ q2.py
@@ -1,2 +1,27 @@
+import re
+
 variable = input("Enter a number: ")
-print(int(variable, base=8))
+
+# float will always have a "." or "e" in it
+# check for float first
+def valid_number():
+    if("." in variable):
+        arr = variable.split(".")
+        if(len(arr)>2):
+            return "None"
+        else:
+            first = arr[0]
+            second = arr[1]
+
+            # if res matches leading zeros, return None
+            res = (re.match("^[-]?[0]{2,}[0-9]*$", first))
+            if(res != None):
+                return "None"
+            res = re.match("^[-]?[0-9]*$", first)
+            if(res == None):
+                return "None"
+            return "good"
+        
+print(valid_number())
+
+
