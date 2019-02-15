dc3ab3f2b985cafb3c16fbdee6f3e2a6a0092436
cwong2015 <sirhc8002@gmail.com>
2019-02-15 13:09:15 -0500

    foolproofed question 2 a little more

--- q2.py
+++ q2.py
@@ -7,22 +7,25 @@ def valid_number():
     if(variable == "0"):
         return "int"
 
+    if(variable == "-"):
+        return "None"
+
     # matches a binary string, and checks for leading zeroes
     if(re.match("^[-]?[0][bB][0]{2,}", variable)):
         return "None"
-    if(re.match("^[-]?[0][bB][01]*$", variable)):
+    if(re.match("^[-]?[0][bB][01]+$", variable)):
         return "int"
 
     # matches an octal string, and checks for leading zeroes
     if(re.match("^[-]?[0][oO][0]{2,}", variable)):
         return "None"
-    if(re.match("^[-]?[0][oO][0-7]*$", variable)):
+    if(re.match("^[-]?[0][oO][0-7]+$", variable)):
         return "int"
 
     # matches a hexadecimal string, and checks for leading zeroes
     if(re.match("^[-]?[0][xX][0]{2,}", variable)):
         return "None"
-    if(re.match("^[-]?[0][xX][A-Fa-f0-9]*$", variable)):
+    if(re.match("^[-]?[0][xX][A-Fa-f0-9]+$", variable)):
         return "int"
 
     # matches a decimal string, and checks for leading zeroes
@@ -32,7 +35,7 @@ def valid_number():
         return "int"
 
     # general case for float first, then prune it after
-    if(re.match("^[-]?[0-9]*[.]?[0-9]*[e]?[-]?[0-9]*$", variable)):
+    if(re.match("^[-]?[0-9]*[.]?[0-9]*[e]?[-]?[0-9]+$", variable)):
         return "float"
     return "None"
 print(valid_number())
