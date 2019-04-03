8e9b8e1e9fca9b5d0ec109b484a965f7481ad939
cwong2015 <sirhc8002@gmail.com>
2019-04-02 22:23:33 -0400

    ok looking good so far.

--- ply_practice/sbml.py
+++ ply_practice/sbml.py
@@ -2,6 +2,9 @@
 Christopher Wong 110410665
 '''
 
+# # # # # # # #
+# data types  #
+# # # # # # # #
 class Node:
     def __init__(self):
         print("init node")
@@ -22,6 +25,26 @@ class NumberNode(Node):
     def evaluate(self):
         return self.value
 
+### Class Tuples(Node):
+
+class ListNode(Node):
+    def __init__(self, v):
+        self.v = [v]
+    def evaluate(self):
+        lst = []
+        for val in self.v:
+            if(isinstance(val, list)):
+                lst.append(val)
+            else:
+                lst.append(val.evaluate())
+        return lst
+
+class StringNode(Node):
+    def __init__(self, v):
+        self.v = str(v[1:-1])
+    def evaluate(self):
+        return self.v
+
 class BooleanNode(Node):
     def __init__(self, v):
         if v=='True':
@@ -31,11 +54,11 @@ class BooleanNode(Node):
     def evaluate(self):
         return self.v
 
-class ListNode(Node):
-    def __init__(self, v):
-        self.v = [v]
-    def evaluate(self):
-        return self.v
+# # # # # #  #
+# operations #
+# # # # # #  #
+
+# class Plus(Node):
 
 class BopNode(Node):
     def __init__(self, op, v1, v2):
@@ -52,35 +75,72 @@ class BopNode(Node):
             return self.v1.evaluate() * self.v2.evaluate()
         elif (self.op == '/'):
             return self.v1.evaluate() / self.v2.evaluate()
+        elif (self.op == 'div'):
+            return self.v1.evaluate() // self.v2.evaluate()
+        elif (self.op == 'mod'):
+            return self.v1.evaluate() % self.v2.evaluate()
+        elif (self.op == '**'):
+            return self.v1.evaluate() ** self.v2.evaluate()
 
 tokens = (
-    'LPAREN', 'RPAREN', 'SEMI',
+    # orelse
+    'OR',   
+    # andalso
+    'AND',
+    # not
+    'NOT',
+    # comparisons
+    'LESSTHAN', 'LESSTHANEQ', 'EQUAL', 'NOTEQ', 'GREATERTHAN', 'GREATERTHANEQ',
+    # con or concatenate aka ::
+    'CON',
+    # in
+    'IN',
+    # +, -
+    'PLUS','MINUS',
+    # *, /, div, mod
+    'TIMES','DIVIDE', 'DIV', 'MOD',
+    # pow
+    'POW',
+    # brackets for indexing
+    'LBRACKET', 'RBRACKET',
+    'LPAREN', 'RPAREN', 
+    # hash
+    'HASH',
+    'COMMA', 'SEMI',
+    # integer, real
     'NUMBER',
-    'PLUS','MINUS','TIMES','DIVIDE',
-    'TRUE', 'FALSE',
-    'LBRACKET', 'RBRACKET'
+    # boolean
+    'TRUE', 'FALSE'
+    # string
+    'STR'
     )
 
-# Tokens
 # t_PRINT    = 'print'
-t_SEMI    = r';'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+t_OR = r'orelse'
+t_AND = r'andalso'
+t_NOT = r'not'
+t_LESSTHAN = r'<'
+t_LESSTHANEQ = r'<='
+t_EQUAL = r'=='
+t_NOTEQ = r'<>'
+t_GREATERTHAN = r'>'
+t_GREATERTHANEQ = r'>='
+t_CON = r'::'
+t_IN = r'in'
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
+t_DIV = r'div'
+t_MOD = r'mod'
+t_POW = r'\*\*'
 t_LBRACKET = r'\['
 t_RBRACKET = r'\]'
-
-def t_TRUE(t):
-    'True'
-    t.value = BooleanNode(t.value)
-    
-
-def t_FALSE(t):
-    'False'
-    t.value = BooleanNode(t.value)
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_HASH = r'\#'
+t_COMMA   = r','
+t_SEMI    = r';'
 
 def t_NUMBER(t):
     r'-?\d*(\d\.|\.\d)\d* | \d+'
@@ -89,6 +149,21 @@ def t_NUMBER(t):
     except ValueError:
         print("Integer value too large %d", t.value)
         t.value = 0
+    return t
+
+def t_TRUE(t):
+    'True'
+    t.value = BooleanNode(t.value)
+    return t
+    
+def t_FALSE(t):
+    'False'
+    t.value = BooleanNode(t.value)
+    return t
+
+def t_STR(t):
+    r'((\"[^\"]*\")|(\'[^\']*\'))'
+    t.value = StringNode(t.value)
     return t
 
 # Ignored characters
@@ -103,16 +178,26 @@ lex.lex()
 
 # Parsing rules
 precedence = (
+    ('left', 'OR'),
+    ('left', 'AND'),
+    ('left', 'NOT'),
+    ('left', 'LESSTHAN', 'LESSTHANEQ', 'EQUAL', 'NOTEQ', 'GREATERTHAN', 'GREATERTHANEQ'),
+    ('right', 'CON'),
+    ('left', 'IN'),
     ('left','PLUS','MINUS'),
-    ('left','TIMES','DIVIDE')
+    ('left','TIMES','DIVIDE', 'DIV', 'MOD'),
+    ('right', 'POW'),
+    ('left', 'LBRACKET', 'RBRACKET'),
+    ('left', 'HASH'),
+    ('left', 'LPAREN', 'RPAREN')
     )
 
 def p_statement_expr(t):
-    'statement : expression'
+    'statement : expression SEMI'
     print(t[1].evaluate())
 
 def p_list(t):
-    '''expression LBRACKET in_list RBRACKET'''
+    '''list : expression LBRACKET in_list RBRACKET'''
     t[0] = t[2]
 
 def p_in_list(t):
@@ -124,10 +209,13 @@ def p_in_list2(t):
     t[0] = t[1].v.append(t[2])
 
 def p_expression_binop(t):
-    '''expression : expression PLUS factor
-                  | expression MINUS factor
-                  | expression TIMES factor
-                  | expression DIVIDE factor'''
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression
+                  | expression DIV expression
+                  | expression MOD expression
+                  | expression POW expression'''
     t[0] = BopNode(t[2], t[1], t[3])
 
 def p_expression_factor(t):
@@ -160,11 +248,8 @@ for line in fd:
         lex.input(code)
         while True:
             token = lex.token()
-            if not token: break
+            if not token: 
+                break
         ast = yacc.parse(code)
     except Exception as e:
         print("SYNTAX ERROR")
-    try:
-        ast.execute()
-    except Exception as e:
-        print("SEMANTIC ERROR", e)
